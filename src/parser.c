4af40538ddae0b108eeac43d3f57a9a94c9a20e9
Jannis <jannis2401p@gmail.com>
2026-06-05 15:02:44 +0200

    removed debug prints and fixxed some typos

--- src/parser.c
+++ src/parser.c
@@ -31,10 +31,7 @@ static inline void advance(Parser *parser) {
 }
 
 static ASTExpr* parse_primary(Parser *parser) {
-    printf("parse primary wurde erreicht. \n");
     ASTExpr* node = malloc(sizeof(ASTExpr));
-    printf("Recieved current token kind: %s \n", token_kind_name(parser->current_token->kind));
-    printf("token value: %s \n", parser->current_token->value);
     switch(parser->current_token->kind) {
         case TOKEN_INT:
             *node = (ASTExpr){.kind = AST_EXPR_INT_LITERAL, .value.literal_value = parser->current_token->value};   
@@ -53,7 +50,7 @@ static ASTExpr* parse_primary(Parser *parser) {
             advance(parser);
         break;
         case TOKEN_TRUE:
-            *node = (ASTExpr){.kind = AST_EXPR_STRING_LITERAL, .value.literal_value = parser->current_token->value};   
+            *node = (ASTExpr){.kind = AST_EXPR_BOOL_LITERAL, .value.literal_value = parser->current_token->value};   
             advance(parser);
         break;
         case TOKEN_FALSE:
@@ -117,6 +114,7 @@ static ASTExpr* parse_primary(Parser *parser) {
             // ident ==> variable
             else {
                 *node = (ASTExpr){.kind = AST_EXPR_VARIABLE, .value.variable_name = parser->current_token->value};   
+                advance(parser);
             }
         break;
         case TOKEN_LPARENT:
@@ -146,15 +144,10 @@ static ASTExpr* parse_primary(Parser *parser) {
 
         break;
         default: 
-            printf("unknwon literal found... \n");
             parser->parser_error = (ParserError){.got = parser->current_token->kind, .token_pos = parser->pos, .status = PARSER_ERR_UNEXPECTED_EXPR_START} ;
             return NULL;
     }
-    printf("parse_primary() hat folgende Node erstellt und allocated: kind = %d, value = %s", node->kind, node->value.literal_value);
-    printf("Alles successful in primary, returning back now.. \n");
-
     if (node == NULL) {
-        printf("node ist irgendwie null ist parse_primary.");
         exit(-1);
     }
 
@@ -188,14 +181,12 @@ static ASTExpr* parse_comparison(Parser *parser) {
     ASTExpr *lhs = parse_primary(parser);
 
     if (lhs == NULL) {
-        printf("lhs in parse_comparison ist null, retuning.. \n");
         return lhs;
     }
 
     TokenKind comparisons[4] = {TOKEN_LT, TOKEN_GT, TOKEN_GTEQ, TOKEN_LTEQ};
 
     while (contains_tokenkind(parser->current_token->kind, comparisons, 4)) {
-        printf("Found comparison, parsing rhs now. \n");
         BinOp op;
 
         switch (parser->current_token->kind) {
@@ -238,12 +229,10 @@ static ASTExpr* parse_comparison(Parser *parser) {
 static ASTExpr* parse_equality(Parser *parser) {
     ASTExpr *lhs = parse_comparison(parser);
     if (lhs == NULL) {
-        printf("lhs ist null in parse_equality, returning..\n");
         return lhs;
     }
 
     while (parser->current_token->kind == TOKEN_EQEQ || parser->current_token->kind == TOKEN_BANGEQ) {
-        printf("Equality wurde gefunden, parse rhs.. \n");
         BinOp op;
         if (parser->current_token->kind == TOKEN_EQEQ) {
             op = BINOP_EQ;
@@ -270,16 +259,13 @@ static ASTExpr* parse_equality(Parser *parser) {
 }
 
 static ASTExpr* parse_and(Parser *parser) {
-    printf("Wir sind jetzt in parse_and() \n");
     ASTExpr* lhs = parse_equality(parser);
 
     if (lhs == NULL) {
-        printf("LHS in parse_and() ist null, retuning.. \n");
         return lhs;
     }
 
     while (parser->current_token->kind == TOKEN_AMP_AMP) {
-        printf("Fond amp amp, parsing RHS in parse_and() now \n");
         advance(parser);
         ASTExpr* rhs = parse_equality(parser);
 
@@ -302,12 +288,10 @@ static ASTExpr* parse_or(Parser *parser) {
     ASTExpr* lhs = parse_and(parser);
 
     if (lhs == NULL) {
-        printf("LHS ist NULL in parse_or(), returning.. \n");
         return lhs;
     }
 
     while (parser->current_token->kind == TOKEN_PIPE_PIPE) {
-        printf("Pipe pipe wurde gefunden, parse rhs now.. \n");
         advance(parser);
         ASTExpr* rhs = parse_and(parser);
 
@@ -327,9 +311,7 @@ static ASTExpr* parse_or(Parser *parser) {
 }
 
 static ASTExpr* parse_expr(Parser *parser) {
-    printf("Wir sind in parse_expr, da diese methode nur ein wrapper ist wird einfach jetzt direkt parse_or aufgerufen! \n");
     ASTExpr* node = parse_or(parser);
-    printf("Parse or ist fertig, wir sind zurück in parse_expr() \n");
     return node;
 }
 
@@ -360,14 +342,7 @@ static void parse_const(Parser *parser) {
     if (!expect(parser, TOKEN_EQ)) return; 
     advance(parser);
 
-    printf("Syntax war okay bis expr, parse expr now! \n");
     ASTExpr* value = parse_expr(parser);
-    printf("Parse expr ist jetzt fertig, wir sind wieder in parse_const angekommen. \n");
-
-    if (value == NULL) {
-        printf("Die expr value ist aber null?!?!? \n");
-    }
-
 
     if (!expect(parser, TOKEN_SEMICOLON)) return;
     advance(parser);
@@ -381,11 +356,9 @@ static void parse_const(Parser *parser) {
         printf("Malloc failed in parse_const() \n");
         exit(1);
     }
-    printf("memory für die ASTConst wurde allocated");
     *c = (ASTConst){.value = value, .name = name, .type = type};
 
     ASTConstVec_push(&parser->ast.constants, c);
-    printf("In den ASTConst vec wurde der ASTConst pointer gepusht. \n");
 }
 
 static void parse_struct(Parser *parser) {
@@ -422,7 +395,6 @@ ParserResult parse_tokens(const TokenVec* tokens) {
                 parse_fn(&parser);
             break;
             case TOKEN_CONST:
-                printf("Found token const on top level, parsing const now..\n");
                 advance(&parser);
                 parse_const(&parser);
             break;
